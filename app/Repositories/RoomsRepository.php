d71c73cf0e08fe6a2fdf8b92ac237342761908c7
Juan Carlos Fuenzalida <juancarlosvillarroel96@gmail.com>
2024-09-04 20:48:21 -0400

    Salas añadidas.

---
+++ app/Repositories/RoomsRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Mail\Rooms\Invite;
+use App\Models\Room;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+
+class RoomsRepository
+{
+    public static function create(User $host): Room
+    {
+        return Room::query()->create([
+            'host_id' => $host->id,
+            'uuid' => Str::uuid(),
+            'token' => Str::random(32),
+        ]);
+    }
+
+    public static function invite(User $guest, Room $room): void
+    {
+        $room->update([
+            'guest_id' => $guest->id,
+        ]);
+
+        Mail::to($guest)->send(new Invite($guest, $room));
+    }
+
+    public static function verify(User $user, string $token, Room $room): bool
+    {
+        return $room->token === $token
+            && $room->guest_id === $user->id;
+    }
+}
